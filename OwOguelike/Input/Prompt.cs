a6a03853548ceade9089cd5b85c7bf59936b8de2
Sprocket <24927632+Sprocket06@users.noreply.github.com>
2022-03-12 04:25:10 -0500

    Misc Fixes so Shit Compiles:TM:

--- OwOguelike/Input/Prompt.cs
+++ OwOguelike/Input/Prompt.cs
@@ -2,7 +2,7 @@ namespace OwOguelike.Input;
 
 public class Prompt
 {
-    public static Texture GetPromptTexture(Player player, Control control)
+    public static Texture GetPromptTexture(Player player, ControlButton control)
     {
         return null!;
     }
