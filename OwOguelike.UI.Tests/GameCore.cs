f699c7732c8c79e2e70b6174c4f302a4b0e0b54f
Enova <elijahbansley@gmail.com>
2022-03-14 13:31:49 -0500

    UI control progress

--- OwOguelike.UI.Tests/GameCore.cs
+++ OwOguelike.UI.Tests/GameCore.cs
@@ -1,6 +1,9 @@
 using System.Drawing;
+using System.Numerics;
 using Chroma;
+using Chroma.Diagnostics;
 using Chroma.Graphics;
+using Color = Chroma.Graphics.Color;
 
 namespace OwOguelike.UI.Tests;
 
@@ -10,12 +13,23 @@ public class GameCore : Game
     
     public GameCore() : base(new(false, false))
     {
+        Graphics.VerticalSyncMode = VerticalSyncMode.None;
+        Graphics.LimitFramerate = false;
+        
         _panel = new Panel(100, 100, 100, 100, Rectangle.FromLTRB(4, 4, 4, 4));
         _panel.AddChild(new Button("owjdnufnwqnfwnfpqwfnwqpofnqwpofqwnfpoquiwfuwoqfhuwhfuwqhfuwqf"));
+        FixedTimeStepTarget = 1;
     }
 
     protected override void Draw(RenderContext context)
     {
+        context.Clear(Color.Green);
         _panel.Draw(context);
+        context.DrawString("test", Vector2.Zero, Color.Red);
+    }
+
+    protected override void FixedUpdate(float delta)
+    {
+        Window.Title = PerformanceCounter.FPS.ToString();
     }
 }
